16a2d7022da7fbe141bdf07d86ca4caf4a9d63ee
Flukena <54875345+Flukena@users.noreply.github.com>
2022-05-21 13:48:53 +0700

    dsa

--- rmovie/vue.config.js
+++ rmovie/vue.config.js
@@ -2,12 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-
   publicPath: '',
   configureWebpack: {
     devServer: {
       historyApiFallback: true,
-      disableHostCheck: true
+      allowedHosts: 'all',
     }
   }
 })
